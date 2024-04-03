67cc85846f0a132d930d99283f6a9ec8887ed2c0
dystein <dystein@syr.edu>
2024-04-03 13:19:07 -0400

    test classes and front-end related API calls

---
+++ frontend/api.js
@@ -0,0 +1,13 @@
+import axios from 'axios';
+
+const API_BASE_URL = 'https://mechanicshopcrm-fff7703161a3.herokuapp.com/';
+
+export const fetchCustomers = async () => {
+  const response = await axios.get(`${API_BASE_URL}/customers`);
+  return response.data;
+};
+
+// You can add more functions here for other operations, like:
+// export const addCustomer = async (customer) => { ... }
+// export const deleteCustomer = async (id) => { ... }
+// etc.
