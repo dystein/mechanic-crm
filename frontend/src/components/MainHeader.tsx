720ae7f5ff25db9d960aeb0dbf8deb2987264cdf
Joshuliu <joshuliu@gmail.com>
2024-04-07 11:58:15 -0400

    Added dummy AddNew popups

--- frontend/src/components/MainHeader.tsx
+++ frontend/src/components/MainHeader.tsx
@@ -1,6 +1,7 @@
 import { FunctionComponent, useState, useCallback, useEffect } from "react";
 import Base from "./Base";
 import { useLocation, useNavigate } from "react-router-dom";
+import styles from "./MainHeader.module.css";
 
 const getTitle = (pathname: string) => {
   if (pathname === "/dashboard" || pathname === "/") {
@@ -34,8 +35,7 @@ const MainHeader: FunctionComponent = () => {
   const [buttonName, setButtonName] = useState("");
 
   // Popup toggle
-  const [showPopup, setShowPopup] = useState(false);
-  const togglePopup = () => setShowPopup(!showPopup);
+  const [showPopup, setShowPopup] = useState(0);
 
   useEffect(() => {
     setTitle(getTitle(location.pathname));
@@ -45,20 +45,41 @@ const MainHeader: FunctionComponent = () => {
     setButtonName(getButton(location.pathname));
   }, [location.pathname]);
 
+  const handleButtonClick = () => {
+    switch (location.pathname) {
+      case "/dashboard":
+        setShowPopup(1); // Show "Add New" popup
+        break;
+      case "/vehicles":
+        setShowPopup(2); // Show "Add New Vehicle" popup
+        break;
+      case "/customers":
+        setShowPopup(3); // Show "Add New Customer" popup
+        break;
+      default:
+        setShowPopup(0); // No popup
+    }
+  };
+  const closePopup = () => setShowPopup(0);
+
   return (
     // changed 5xl to black
     <div className="fixed w-full top-[0px] right-[0px] left-[0px] h-[90px] text-5xl text-black">
+      {/* Logo */}
+      <img
+        className="absolute top-[calc(50%_-_45px)] left-[0px] w-[90px] h-[90px] overflow-hidden cursor-pointer"
+        alt=""
+        src="/logo.svg"
+      />
+      {/* Header Left */}
       <div className="absolute w-[calc(100%_-_507px)] top-[0px] right-[417px] left-[90px] bg-ghostwhite h-[90px] overflow-hidden">
         <b className="absolute top-[calc(50%_-_20px)] left-[24px] leading-[40px]">
           {title}
         </b>
       </div>
+      {/* Header Right */}
       <div className="absolute top-[0px] right-[0px] bg-ghostwhite w-[417px] h-[90px] overflow-hidden">
-        <img
-          className="absolute top-[calc(50%_-_25px)] right-[94px] rounded-31xl w-[50px] h-[50px] overflow-hidden"
-          alt=""
-          src="button.svg"
-        />
+        {/* Add New (Vehicle or Customer) Button */}
         {location.pathname !== "/vehicle-detail" &&
           location.pathname !== "/customer-detail" && (
             <button
@@ -71,7 +92,7 @@ const MainHeader: FunctionComponent = () => {
                 right: "164px",
                 cursor: "pointer",
               }}
-              onClick={togglePopup}
+              onClick={handleButtonClick}
             >
               <div className="relative leading-[30px] font-medium">
                 {buttonName}
@@ -79,17 +100,36 @@ const MainHeader: FunctionComponent = () => {
               <img className="w-5 relative h-5" alt="" src="/icon.svg" />
             </button>
           )}
+        {/* Search Button */}
+        <img
+          className="absolute top-[calc(50%_-_25px)] right-[94px] rounded-31xl w-[50px] h-[50px] overflow-hidden"
+          alt=""
+          src="button.svg"
+        />
+        {/* Account Profile Picture */}
         <img
           className="absolute top-[calc(50%_-_25px)] right-[24px] rounded-81xl w-[50px] h-[50px] overflow-hidden object-cover"
           alt=""
           src="/user1@2x.png"
         />
       </div>
-      <img
-        className="absolute top-[calc(50%_-_45px)] left-[0px] w-[90px] h-[90px] overflow-hidden cursor-pointer"
-        alt=""
-        src="/logo.svg"
-      />
+      {showPopup === 1 && (
+        <div className={styles.popup}>
+          <button onClick={() => setShowPopup(2)}>Add New Vehicle</button>
+          <button onClick={() => setShowPopup(3)}>Add New Customer</button>
+          <button onClick={closePopup}>Close</button>
+        </div>
+      )}
+      {showPopup === 2 && (
+        <div className={styles.popup}>
+          Add New Vehicle Form <button onClick={closePopup}>Close</button>
+        </div>
+      )}
+      {showPopup === 3 && (
+        <div className={styles.popup}>
+          Add New Customer Form <button onClick={closePopup}>Close</button>
+        </div>
+      )}
       <Base
         baseWidth="100%"
         basePosition="absolute"
